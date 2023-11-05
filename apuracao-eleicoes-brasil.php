c4932c27e13875e161af8000cd572e59f0feca0a
Anderson <117995374+andersoncgpb1@users.noreply.github.com>
2023-11-05 20:35:54 -0300

    Add files via upload

--- apuracao-eleicoes-brasil.php
+++ apuracao-eleicoes-brasil.php
@@ -2,24 +2,23 @@
 /*
 Plugin Name: Apuracao Eleicoes Brasil
 Plugin URI: https://github.com/valeriosouza/apuracao-eleicoes-brasil
-Description: Apuração das Eleições 2014 do Brasil
-Version: 0.2.1
-Author: Valerio Souza
-Author URI: http://valeriosouza.com.br
+Description: Apuração das Eleições do Brasil
+Version: 1.0
+Author: Anderson Souza
+Author URI: https://instagram.com/andersoncgpb
 Text Domain: eleicoes-brasil
 Domain Path: /languages
 */
 
 // Add Shortcode
 function apura_tse() {?>
-<iframe
-src="http://divulga.tse.jus.br/oficial/index.html" height="700" width="100%" frameborder="0" scrolling="auto">You need an iframes capable browser to view this content.</iframe>
+<iframe src="https://resultados.tse.jus.br/oficial/app/index.html" height="700" width="100%" frameborder="0" scrolling="auto">You need an iframes capable browser to view this content.</iframe>
 <?php 
 }
 add_shortcode( 'apuracao', 'apura_tse' );
 
 function apura_twitter() {?>
-<a class="twitter-timeline" href="https://twitter.com/search?q=elei%C3%A7%C3%B5es+OR+elei%C3%A7%C3%B5es2014+OR+eleicoes+lang%3Apt" data-widget-id="519666914453233664">Tweets sobre "eleições OR eleições2014 OR eleicoes lang:pt"</a>
+<a class="twitter-timeline" href="https://twitter.com/search?q=elei%C3%A7%C3%B5es%20OR%20elei%C3%A7%C3%B5es2024%20OR%20eleicoes%20lang%3Apt&src=typed_query&f=top" data-widget-id="519666914453233664">Tweets sobre "eleições OR eleições2024 OR eleicoes lang:pt"</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 <?php 
 }
